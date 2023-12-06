0fbfa1066639f945ff135029dd5b7e8da4832af9
Gabriel Rey <rgabi1219@gmail.com>
2023-12-06 15:49:13 -0300

    Definimos el modelo publicaciones y creamos una view para leer las publicaciones y renderizarlas en el template.

--- blog_prueba/publicaciones/urls.py
+++ blog_prueba/publicaciones/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ver-publicaciones/', views.publicaciones_views, name='publicaciones')
+    path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones')
 ]
--- blog_prueba/publicaciones/views.py
+++ blog_prueba/publicaciones/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import Publicacion
+from django.views.generic import ListView
 
 # Create your views here.
 
-def publicaciones_views(request):
+#View basa en función, para listar publicaciones.
+"""def publicaciones_views(request):
 
     ctx = {
         'publicaciones' : Publicacion.objects.all()
     }
 
-    return render(request, 'publicaciones.html', ctx)
+    return render(request, 'publicaciones.html', ctx)"""
+
+# View basa en clase, para listar las publicaciones.
+class PublicacionesView(ListView):
+    template_name = 'publicaciones.html'
+    model = Publicacion
+    context_object_name = 'publicaciones'
