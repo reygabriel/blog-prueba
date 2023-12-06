748003e853fb8d0303cda2009cc6afd17fd797dd
Gabriel Rey <rgabi1219@gmail.com>
2023-12-06 17:35:48 -0300

    Ya está creado el formulario para hacer publicaciones y funciona.

--- blog_prueba/publicaciones/urls.py
+++ blog_prueba/publicaciones/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones')
+    path('ver-publicaciones/', views.PublicacionesView.as_view(), name='publicaciones'),
+    path('publicar/', views.publicar_view, name='publicar')
 ]
--- blog_prueba/publicaciones/views.py
+++ blog_prueba/publicaciones/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Publicacion
 from django.views.generic import ListView
+from .forms import PublicarForm
 
 # Create your views here.
 
@@ -15,6 +16,20 @@ from django.views.generic import ListView
 
 # View basa en clase, para listar las publicaciones.
 class PublicacionesView(ListView):
-    template_name = 'publicaciones.html'
+    template_name = 'publicaciones/publicaciones.html'
     model = Publicacion
     context_object_name = 'publicaciones'
+
+# View basada en función, para crear las publicaciones
+def publicar_view(request):
+    if request.method == "POST":
+        # Guarda la data del formulario en la tabla
+        form = PublicarForm(request.POST)
+        if form.is_valid():
+            nueva_publicacion = form.save()
+        return redirect('publicaciones')
+    else:
+        # Renderizar el html
+        form = PublicarForm()
+        ctx = {'form' : form}
+        return render(request, 'publicaciones/publicar.html', ctx)
