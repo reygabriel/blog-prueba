d0e2238c5a340153f56efc8c6aa9c8a6a7f62851
Gabriel Rey <rgabi1219@gmail.com>
2023-12-07 17:04:24 -0300

    No se que agregué pero no era importante

--- blog_prueba/usuarios/models.py
+++ blog_prueba/usuarios/models.py
@@ -7,5 +7,5 @@ class Usuario(AbstractUser):
     domicilio = models.CharField(max_length=100, null=True, blank=True)
     es_colaborador = models.BooleanField(default=False)
 
-def __str__(self):
-    return self.first_name + " " + self.last_name
+    def __str__(self):
+        return self.first_name + " " + self.last_name
